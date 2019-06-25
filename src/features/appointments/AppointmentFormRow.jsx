3e5a513727d41b889366f6e6b4d92e3f064929d7
Veronica Mihai <mihaiveronica@ymail.com>
2019-06-25 21:45:25 +0100

    BookAppointmentForm --> AppointmentFormRow Component

--- src/features/appointments/AppointmentFormRow.jsx
+++ src/features/appointments/AppointmentFormRow.jsx
@@ -6,15 +6,18 @@ import './BookAppointmentForm.scss';
 // Utilities
 import classNames from 'classnames';
 
+// import classNames from 'classnames';
+
 const AppointmentFormRow = ({icon, title, children}) => {
 	const iconClasses = classNames(icon, 'form-row__icon');
-
 	return (<div className="form-row__container"> 
 		<div className="form-row__header"> 
 			<i className={iconClasses} />
-			<div> {title} </div>
+			<div className="form-row__title"> {title} </div>
 		</div>
-		{children}
+		<div className="form-row__children"> 
+			{children}
+		</div>
 	</div>);
 };
 
