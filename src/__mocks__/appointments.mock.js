3478191a21cdc46f4dc135f711ebb414ac38bac0
Veronica Mihai <mihaiveronica@ymail.com>
2019-06-26 21:19:49 +0100

    Mocks --> axios, appointments and user

---
+++ src/__mocks__/appointments.mock.js
@@ -0,0 +1,115 @@
+const availableSlots = [
+  {
+    "id": 1,
+    "consultantType": [
+      "gp"
+    ],
+    "appointmentType": [
+      "audio",
+      "video"
+    ],
+    "time": "2019-11-27T10:11:00.000Z"
+  },
+  {
+    "id": 2,
+    "consultantType": [
+      "specialist",
+      "gp"
+    ],
+    "appointmentType": [
+      "audio",
+      "video"
+    ],
+    "time": "2019-12-01T14:16:30.000Z"
+  },
+  {
+    "id": 3,
+    "consultantType": [
+      "gp"
+    ],
+    "appointmentType": [
+      "audio",
+      "video"
+    ],
+    "time": "2019-10-08T16:17:30.000Z"
+  },
+  {
+    "id": 4,
+    "consultantType": [
+      "therapist",
+      "gp"
+    ],
+    "appointmentType": [
+      "audio",
+      "video"
+    ],
+    "time": "2019-11-16T16:18:30.000Z"
+  },
+  {
+    "id": 5,
+    "consultantType": [
+      "specialist"
+    ],
+    "appointmentType": [
+      "audio",
+      "video"
+    ],
+    "time": "2019-12-26T17:19:00.000Z"
+  },
+  {
+    "id": 6,
+    "consultantType": [
+      "gp"
+    ],
+    "appointmentType": [
+      "audio",
+      "video"
+    ],
+    "time": "2019-09-23T18:20:00.000Z"
+  },
+  {
+    "id": 7,
+    "consultantType": [
+      "gp"
+    ],
+    "appointmentType": [
+      "audio",
+      "video"
+    ],
+    "time": "2019-08-30T19:21:30.000Z"
+  }
+];
+
+const gpAvailableSlots = [{
+    "label": "Wed Nov 27 2019, 10:11",
+    "value": "2019-11-27T10:11:00.000Z"
+  },
+  {
+    "label": "Sun Dec 01 2019, 14:16",
+    "value": "2019-12-01T14:16:30.000Z"
+  },
+  {
+    "label": "Tue Oct 08 2019, 17:17",
+    "value": "2019-10-08T16:17:30.000Z"
+  },
+  {
+    "label": "Sat Nov 16 2019, 16:18",
+    "value": "2019-11-16T16:18:30.000Z"
+  },
+  {
+    "label": "Mon Sep 23 2019, 19:20",
+    "value": "2019-09-23T18:20:00.000Z"
+  },
+  {
+    "label": "Fri Aug 30 2019, 20:21",
+    "value": "2019-08-30T19:21:30.000Z"
+  },
+  {
+    "label": "Another time",
+    "value": "anotherTime"
+  }];
+
+export default {
+	availableSlots,
+	gpAvailableSlots
+};
